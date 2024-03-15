2f0b1b5aa5a54561662cc14cdcf2cc5049470d47
juarezLSantos <160648703+juarezLSantos@users.noreply.github.com>
2024-03-15 17:38:24 +0000

    Notas

---
+++ Notas.js
@@ -0,0 +1,17 @@
+// Detalhe de notas
+const nota1 = 10;
+const nota2 = 10;
+const nota3 = 10;
+
+// calculo da media
+const media = (nota1 + nota2 + nota3) / 3;
+
+// estrutura de decisão composta
+if (media >= 7) {
+    console.log("Aluno aprovado!");
+}else if (media <= 4) {
+    console.log("Aluno reprovado!");
+}else {
+    console.log("Aluno em exame!");
+
+}
